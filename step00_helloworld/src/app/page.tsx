cceaa7de20a717d2534c34a6fafa8c8082981820
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-09-02 00:11:40 +0500

    first-commit

---
+++ step00_helloworld/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="text-cyan-50 text-center">
+      <h1 className="text-3xl">Hello Next JS</h1>
+      <h2 className="text-xl">From Nabeel Ali</h2>
+    </div>
+  );
+}
